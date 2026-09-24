c4f511723edfa20a1a77075d76f44263b118bcbd
Martin Morales <martinandresmm10@gmail.com>
2026-09-24 08:10:08 -0600

    agreaga cambios (no hacer merge con main)

--- Proyecto_1/obj/Debug/net9.0/Proyecto_1.AssemblyInfo.cs
+++ Proyecto_1/obj/Debug/net9.0/Proyecto_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fdc35df5d43ffa13b524ead7b24a81eef6b01e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f4baa946fbbd8e70dd60ed8e9718f6c0d16a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
